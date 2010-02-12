6abf0dcf000eb21bb96dcfd8ced7e7be6a5c98e3
Nick Anderson <cmdln@tp.(none)>
2010-02-11 18:02:32 -0600

    Refactored parsing, added error checking, renamed file

--- check_sar_perf.py
+++ check_sar_perf.py
@@ -1,27 +1,70 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os
+import sys
 from subprocess import *
 
 
 os.environ['PATH'] = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin'
 
+class SarNRPE:
 
-class Paging:
-    def __init__(self):
-        sar=Popen('sar -B 1 1', shell=True, stdout=PIPE, stderr=PIPE)
+    '''Call sar and parse statistics returning in NRPE format'''
+    def __init__(self, command, handler):
+        sar=Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
         (sout,serr) = sar.communicate()
-        self.Average = sout.split('\n')[-2]
-        self.pgpgin = self.Average.split()[1]
-        self.pgpgout = self.Average.split()[2]
-        self.fault = self.Average.split()[3]
-        self.majflt = self.Average.split()[4]
-        self.pgfree = self.Average.split()[5]
-        self.pgscank = self.Average.split()[6]
-        self.pgscand = self.Average.split()[7]
-        self.pgsteal = self.Average.split()[8]
-        self.vmeff = self.Average.split()[9]
+        parser = getattr(self, handler)
+        if callable(parser):
+            parser(sout)
+        else:
+            print 'ERROR: parser does not exist'
+            sys.exit(1)
+
+    def standard(self, sout):
+        '''Standard parser for non multi-device output'''
+        self.Average = sout.split('\n')[-2].split()
+        # remove 'Average:'
+        self.Average.pop(0)
+        self.Columns = sout.split('\n')[-4].split()
+        # Remove Timestamp
+        self.Columns.pop(0)
+        self.Columns.pop(0)
+        self.stats = []
+        # Create dictionary
+        for i in range(len(self.Columns)):
+            string = "'%s'=%s" %(self.Columns[i], self.Average[i])
+            self.stats.append(string)
+
+def CheckBin(program):
+    for path in os.environ.get('PATH', '').split(':'):
+        if os.path.exists(os.path.join(path, program)) and \
+           not os.path.isdir(os.path.join(path, program)):
+               return os.path.join(path, program)
+               #return True
+    return False
 
 
-stat = Paging()
-print stat.Average
-print stat.pgpgin,stat.pgpgout,stat.fault,stat.majflt,stat.pgfree,stat.pgscank,stat.pgscand,stat.pgsteal,stat.vmeff
+def Main(args):
+    if not CheckBin('sar'):
+        print 'ERROR: sar not found on PATH (%s), install sysstat' %os.environ['PATH']
+        sys.exit(1)
+    myOpts = {}
+    myOpts['paging'] = 'sar -B 1 1'
+    myOpts['cpu'] = 'sar -C 1 1'
+    myOpts['memory_util'] = 'sar -r 1 1'
+    myOpts['memory_stat'] = 'sar -R 1 1'
+    myOpts['io_transfer'] = 'sar -b 1 1'
+    myOpts['queueln_load'] = 'sar -q 1 1'
+    myOpts['swap_util'] = 'sar -S 1 1'
+    myOpts['swap_stat'] = 'sar -W 1 1'
+    myOpts['kernel'] = 'sar -v 1 1'
+
+    if args[1] in myOpts:
+        sar = SarNRPE(myOpts[args[1]],'standard')
+    else:
+        print 'ERROR: option not defined'
+        sys.exit(1)
+    # Output in NRPE format
+    print '|', ' '.join(sar.stats)
+
+if __name__ == '__main__':
+    Main(sys.argv)
