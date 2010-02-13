2b25a07466fc5179ecd8c30f6602ab5d98c97ea9
Nick Anderson <cmdln@tp.(none)>
2010-02-13 00:05:32 -0600

    Added License (bsd-new), inline documentation, and refactored a few variable names to be more descriptive.

--- check_sar_perf.py
+++ check_sar_perf.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# Copyright (c) 2010, Nick Anderson <nick@cmdln.org>
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import sys
 import re
@@ -16,6 +38,10 @@ os.environ['PATH'] = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin'
 #                it from performing the specified operation. Higher-level errors (such as name 
 #                resolution errors, socket timeouts, etc) are outside of the control of plugins and 
 #                should generally NOT be reported as UNKNOWN states. 
+ERR_OK = 0
+ERR_WARN = 1
+ERR_CRIT = 2
+ERR_UNKN = 3
 
 class SarNRPE:
     '''Call sar and parse statistics returning in NRPE format'''
@@ -27,9 +53,10 @@ class SarNRPE:
         else:
             (columns, data) = self.SortCombinedOutput(sout, device)
 
-        self.Parser(columns, data)
+        self.Formatter(columns, data)
 
     def SortOutput(self, sarout):
+        '''Sort output of sar command, return column and data tuple'''
         data = sarout.split('\n')[-2].split()
         # remove 'Average:'
         data.pop(0)
@@ -40,10 +67,12 @@ class SarNRPE:
         return (columns, data)
 
     def SortCombinedOutput(self, sarout, device):
-        '''Sort output from sar command, return columns and relevant data'''
+        '''Sorts column and data output from combined report and displays
+        only relevant information returns column and data tuple'''
         find_columns = True
         mycolumns = []
         mydata = []
+        # Find the column titles
         search = re.compile('^Average:')
         for line in sarout.split('\n'):
             if search.match(line):
@@ -52,6 +81,7 @@ class SarNRPE:
                     find_columns = False
                 else:
                     mydata.append(line)
+        # Find the only Average line with the device we are looking for
         search = re.compile('^Average:\s.*%s\s.*' %device)
         for line in mydata:
             if not search.match(line):
@@ -62,8 +92,8 @@ class SarNRPE:
         mydata.pop(0)
         return (mycolumns,mydata)
 
-    def Parser(self, columns, data):
-        '''Sar parser to construct nrpe formatted device'''
+    def Formatter(self, columns, data):
+        '''Construct nrpe format performance data'''
         self.stats = []
         # Create dictionary
         for i in range(len(columns)):
@@ -71,6 +101,7 @@ class SarNRPE:
             self.stats.append(string)
 
 def CheckBin(program):
+    '''Ensure the program exists in the PATH'''
     for path in os.environ.get('PATH', '').split(':'):
         if os.path.exists(os.path.join(path, program)) and \
            not os.path.isdir(os.path.join(path, program)):
@@ -80,13 +111,16 @@ def CheckBin(program):
 
 
 def Main(args):
+    # Ensure a profile (aka myOpts) is selected
     if not len(args) > 1:
         print 'ERROR: no profile selected'
-        sys.exit(3)
+        sys.exit(ERR_UNKN)
     if not CheckBin('sar'):
         print 'ERROR: sar not found on PATH (%s), install sysstat' %os.environ['PATH']
-        sys.exit(2)
-
+        sys.exit(ERR_CRIT)
+  
+    # Profiles may need to be modified for different versions of the sysstat package
+    # This would be a good candidate for a config file 
     myOpts = {}
     myOpts['paging'] = 'sar -B 1 1'
     myOpts['cpu'] = 'sar -C 1 1'
@@ -96,21 +130,24 @@ def Main(args):
     myOpts['queueln_load'] = 'sar -q 1 1'
     myOpts['swap_util'] = 'sar -S 1 1'
     myOpts['swap_stat'] = 'sar -W 1 1'
+    myOpts['task'] = 'sar -w 1 1'
     myOpts['kernel'] = 'sar -v 1 1'
     myOpts['disk'] = 'sar -d -p 1 1'
-
+    
+    # If profile uses combined output you must pick one device to report on ie sda for disk
     if args[1] in myOpts:
         if args[1] == 'disk':
             if len(args) > 2:
                 sar = SarNRPE(myOpts[args[1]],args[2])
             else:
                 print 'ERROR: no device specified'
-                sys.exit(3)
+                sys.exit(ERR_UNKN)
         else:
             sar = SarNRPE(myOpts[args[1]])
     else:
         print 'ERROR: option not defined'
-        sys.exit(3)
+        sys.exit(ERR_UNKN)
+
     # Output in NRPE format
     print 'sar OK|', ' '.join(sar.stats)
 
